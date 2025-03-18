649f75babd9338acb75bec841f62189afffa2cd1
Hugo Avallay <h40hugo@gmail.com>
2025-03-18 00:51:57 -0300

    se implemento la función para agregar amigos desde el input

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+
+
+let listaAmigos = [];
 
 function agregarAmigo() {
-    alert("clic fala")
+
+    let inputAmigo = document.getElementById("amigo");
+    let nombre = inputAmigo.value.trim(); 
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    listaAmigos.push(nombre);
+
+    actualizarLista();
+
+    inputAmigo.value = "";
 }
+
+function actualizarLista() {
+    let listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = ""; 
+    listaAmigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    });
+}
+
+
